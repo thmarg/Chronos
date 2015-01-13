f7ab5c5084749086f39ef9af55775995db4700d4
Thierry Margenstern <thmarg666@gmail.com>
2015-01-13 20:48:05 +0100

    Code update to use generic class ChronometerActivity

--- src/tm/android/chronos/activity/Chronos.java
+++ src/tm/android/chronos/activity/Chronos.java
@@ -1,5 +1,5 @@
 /*
- * Chronos
+ * $SRC
  *
  *   Copyright (c) 2014 Thierry Margenstern under MIT license
  *   http://opensource.org/licenses/MIT
@@ -13,7 +13,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import tm.android.chronos.R;
+import tm.android.chronos.core.Stopwatch;
+
 
 /**
  * Entry point of the Application
@@ -24,32 +27,36 @@ public class Chronos extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chronos);
 
-        
+
     }
 
 
-
+    @SuppressWarnings("unchecked")
     public void onClick(View view) {
-            if (view.getId()==R.id.btn_StopwatchAcitvity){
-                Intent intent = new Intent(getBaseContext(),ChronometerActivity.class);
+        if (view.getId()==R.id.btn_StopwatchAcitvity){
+            try {
+                Class<ChronometerActivity<Stopwatch>> cl = (Class<ChronometerActivity<Stopwatch>>)Class.forName("tm.android.chronos.activity.ChronometerActivity");
+                Intent intent = new Intent(getBaseContext(),cl);
                 startActivity(intent);
-            } else {
-                Dialog dialog = new Dialog(this);
-                TextView textView = new TextView(this);
-                textView.setText("Not yet implemented !\nComing soon.");
-                textView.setTextAppearance(getBaseContext(),android.R.style.TextAppearance_Large);
-                dialog.setContentView(textView);
-                //dialog.setContentView(R.layout.layoutforspinner);
-                dialog.setTitle("Message-oup!!!");
-                dialog.show();
+            } catch (ClassNotFoundException e){
+                Toast.makeText(this,"Unable to instantiate ChronometerActivity",Toast.LENGTH_LONG).show();
             }
+        } else {
+            Dialog dialog = new Dialog(this);
+            TextView textView = new TextView(this);
+            textView.setText("Not yet implemented !\nComing soon.");
+            textView.setTextAppearance(getBaseContext(),android.R.style.TextAppearance_Large);
+            dialog.setContentView(textView);
+            dialog.setTitle("Message-oup!!!");
+            dialog.show();
+        }
     }
-    
-    
-    
+
+
+
 }
 
 
 
 
-    
+

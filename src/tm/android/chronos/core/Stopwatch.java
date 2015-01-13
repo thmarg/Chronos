bb2b7826d8b1f11a0a0a59435497fb1dd36fccb6
Thierry Margenstern <thmarg666@gmail.com>
2015-01-13 19:30:14 +0100

    Change type to Interface due to new design

--- src/tm/android/chronos/core/Stopwatch.java
+++ src/tm/android/chronos/core/Stopwatch.java
@@ -1,106 +1,45 @@
 /*
- *   Stopwatch
+ * Stopwatch
  *
- *   Copyright (c) 2014 Thierry Margenstern under MIT license
- *   http://opensource.org/licenses/MIT
+ * Copyright (c) 2014 Thierry Margenstern under MIT license
+ * http://opensource.org/licenses/MIT
  */
 
 package tm.android.chronos.core;
 
 /**
- *
+ * Some other methods for a stopwatch.
  */
-public class Stopwatch extends AbstractClock {
+public interface Stopwatch extends Clock{
+    StopwatchData getStopwatchData();
 
-    private StopwatchData stopwatchData;
-    private StopwatchRenderDelegate<Digit> stopwatchRenderDelegate;
-    private boolean noRunRenderUpdate = true;
+    /**
+     * Call this method to register a lap time
+     * @param now time of the action
+     */
+    void lapTime(long now);
 
+    /**
+     * *
+     * @return true if some info other than time have changed
+     */
+    boolean mustUpdateUI();
 
-    public Stopwatch(StopwatchRenderDelegate<Digit> stopwatchRenderDelegate) {
-        super();
-        stopwatchData = new StopwatchData(getName());
-        this.stopwatchRenderDelegate = stopwatchRenderDelegate;
+    /**
+     * *
+     * @param must update ui or not.
+     */
+    void setMustUpdateUI(boolean must);
 
-    }
+    /**
+     * Call this method when this stopwatch must be deleted (in the business logic and on UI)
+     */
+    void setMustDelete();
 
+    /**
+     * Say if this stopwatch must be deleted or not.
+     * @return boolean
+     */
+    boolean mustDelete();
 
-    @Override
-    protected void innerRun() throws InterruptedException {
-        sleep(20);
-        long diff = System.currentTimeMillis() - lastTime;
-        currentTime.addMillisSeconds(diff);
-        lastTime += diff;
-        stopwatchRenderDelegate.doDrawRun(currentTime);
-    }
-
-    @Override
-    protected void noRunRender() {
-        stopwatchRenderDelegate.doDrawNoRun(currentTime, noRunRenderUpdate);
-        noRunRenderUpdate = false;
-
-    }
-
-    @Override
-    protected void finalRun() {
-        stopwatchRenderDelegate.doFinal();
-    }
-
-    @Override
-    protected void innerStop() {
-        try {
-            sleep(20);
-        } catch (InterruptedException e) {
-            //
-        }
-        currentTime = Digit.split(stopTime - startTime);
-        stopwatchData.setChronoTime(currentTime.getInternal());
-
-        noRunRenderUpdate = true;
-
-    }
-
-    public void intermediateTime(long now) {
-        stopwatchData.add(now - startTime);
-    }
-
-    public StopwatchData getStopwatchData() {
-        return stopwatchData;
-    }
-
-    @Override
-    protected void innerPause() {
-
-    }
-
-    @Override
-    protected void innerRestart() {
-
-    }
-
-    @Override
-    protected void innerReset() {
-        stopwatchData.reset();
-        noRunRenderUpdate = true;
-
-    }
-
-    public void setNoRunRenderUpdate(boolean noRunRenderUpate) {
-        this.noRunRenderUpdate = noRunRenderUpate;
-    }
-
-
-    public Stopwatch RebuildAndStart(){
-        Stopwatch ret = new Stopwatch(stopwatchRenderDelegate);
-        ret.startTime = startTime;
-        ret.currentTime = Digit.split(0);
-        ret.lastTime = startTime;
-        ret.setName(getName());
-        ret.setNoRunRenderUpdate(false);
-        ret.setRunning();
-        ret.startLoop();
-
-        return ret;
-
-    }
 }

33f2b3cbed4d12448bf8f2907ecb2e72b8a3a0ec
Thierry Margenstern <thmarg666@gmail.com>
2015-01-22 18:23:16 +0100

    Change method signature

--- src/tm/android/chronos/core/SurfaceViewRenderer.java
+++ src/tm/android/chronos/core/SurfaceViewRenderer.java
@@ -10,14 +10,13 @@ package tm.android.chronos.core;
 import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
-import java.util.Vector;
 
 /**
  *  Implement this so that a thread can render on a surface view and with
  *  a cache system which make easy to do and control scrolling.
  */
-public interface SurfaceViewRenderer<T extends Clock> {
+public interface SurfaceViewRenderer {
     SurfaceHolder getHolder();
-    void renderOnCache(Vector<T> clockList);
+    void renderOnCache();
     void renderOnScreen(Canvas canvas);
 }
